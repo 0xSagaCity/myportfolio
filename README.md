7b2d438e02a09ab9cc4e01cefc12fa902812c171
0xSagaCity <dragondive42@gmail.com>
2023-05-10 19:16:11 +0530

    Proper README.md added

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">Portfolio Website</h1>
+
+<p>
+This is my personal portfolio website (basically propaganda for recruiters).
+It was designed by myself in figma and has some interesting cursor effect I have been poking around for a while.
+</p>
+
+[Live Site](https://pavitrakar.netlify.app/)
+
+<b><code>Screenshots (Light)</code></b>
+
+| Aboutme Page                            | Work Page                               |
+| --------------------------------------- | --------------------------------------- |
+| ![img](https://i.imgur.com/UQM0HXI.png) | ![img](https://i.imgur.com/zbt5EZL.png) |
+
+
+<b><code>Screenshots (Dark)</code></b>
+
+| Aboutme Page                            | Work Page                               |
+| --------------------------------------- | --------------------------------------- |
+| ![img](https://i.imgur.com/xpph9X5.png) | ![img](https://i.imgur.com/QppqPNP.png) |
--- src/utils/projectData.tsx
+++ src/utils/projectData.tsx
@@ -14,7 +14,7 @@ export const projectData = [
         description:
             "The Froots Interface project is a UI design for an imaginary e-commerce website that sells fruits, with the playfulness of the name Froots reflected in the design. It is built using Framer Motion, React, and CSS, and features smooth animations.",
         linkSource: "https://github.com/0xSagaCity/Froots",
-        linkLive: "https://0xsagacity.github.io/Froots/",
+        linkLive: "https://froots.netlify.app/",
         thumbnail: "https://i.imgur.com/FRVzu1m.png",
         icons: [
             {
@@ -39,8 +39,8 @@ export const projectData = [
         title: "Multiplayer Tic Tac Toe",
         description:
             "This is a multiplayer Tic Tac Toe project that uses PeerJS for peer-to-peer communication. The project is built with React, PeerJS, GSAP, and SASS. The game allows two players to compete against each other in a game of Tic Tac Toe in real-time. GSAP is used for animations, while SASS is used for styling.",
-        linkSource: "https://www.example.com/projectone",
-        linkLive: "https://multiplayer-tic-tac-toe.surge.sh/",
+        linkSource: "https://github.com/0xSagaCity/tic-tac-toe",
+        linkLive: "https://t3-online.netlify.app/",
         thumbnail: "https://i.imgur.com/n5fmZDR.png",
         icons: [
             {
@@ -71,7 +71,7 @@ export const projectData = [
             "This is a tea manufacturer's homepage designed with a parallax effect. The page is built using React, CSS, and Framer Motion to create an engaging and immersive user experience. Visitors can learn about the tea manufacturer and their products through the visually stunning parallax effect.",
         linkSource:
             "https://github.com/0xSagaCity/Tea-Manufacturer-React-Animations/",
-        linkLive: "https://tea-manufacturer.surge.sh/",
+        linkLive: "https://tea-manufacturer.netlify.app/",
         thumbnail: "https://i.imgur.com/KiaPH95.png",
         icons: [
             {
